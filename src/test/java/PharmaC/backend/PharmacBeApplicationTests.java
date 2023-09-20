d7f7d896a9f35321655fed76c7349d57ab2a44f1
chaeshee0908 <chaeshee0908@naver.com>
2023-09-20 15:55:51 +0900

    [init] 파마씨 프로젝트 생성

---
+++ src/test/java/PharmaC/backend/PharmacBeApplicationTests.java
@@ -0,0 +1,13 @@
+package PharmaC.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
