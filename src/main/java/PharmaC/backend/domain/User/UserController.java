1de2fecd172cb015a33580509ade1d028f6bf5cf
chaeshee0908 <chaeshee0908@naver.com>
2023-10-25 23:28:30 +0900

    [fix] 유지 매핑 테이블 이름 수정

--- src/main/java/PharmaC/backend/domain/User/UserController.java
+++ src/main/java/PharmaC/backend/domain/User/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/user")
-@Tag(name = "User")
+@Tag(name = "Users")
 public class UserController {
 
     private final UserService userService;
