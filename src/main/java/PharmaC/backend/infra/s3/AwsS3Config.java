2555cafcbf9acca601a7e9cd60919a02c0717936
chaeshee0908 <chaeshee0908@naver.com>
2023-10-25 20:36:52 +0900

    [feat] s3 기능 구현 #15

---
+++ src/main/java/PharmaC/backend/infra/s3/AwsS3Config.java
@@ -0,0 +1,29 @@
+package PharmaC.backend.infra.s3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+public class AwsS3Config {
+    @Value("${cloud.aws.credentials.access-key}")
+    private String AWS_ACCESS_KEY_ID;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String AWS_SECRET_KEY;
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        final AwsBasicCredentials awsBasicCredentials =
+                AwsBasicCredentials.create(AWS_ACCESS_KEY_ID, AWS_SECRET_KEY);
+        final StaticCredentialsProvider staticCredentialsProvider =
+                StaticCredentialsProvider.create(awsBasicCredentials);
+        return S3Presigner.builder()
+                .credentialsProvider(staticCredentialsProvider)
+                .region(Region.AP_NORTHEAST_2)
+                .build();
+    }
+}
---
+++ src/main/java/PharmaC/backend/infra/s3/AwsS3UrlGenerator.java
@@ -0,0 +1,37 @@
+package PharmaC.backend.infra.s3;
+
+import java.time.Duration;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+@Component
+@RequiredArgsConstructor
+public class AwsS3UrlGenerator {
+    @Value("${cloud.aws.s3.bucket}")
+    private String BUCKET;
+
+    private final AwsS3Config awsS3Config;
+
+    public String generateUrl(String prefix) {
+        String filename = UUID.randomUUID().toString();
+
+        PutObjectRequest objectRequest =
+                PutObjectRequest.builder().bucket(BUCKET).key(prefix + "/" + filename).build();
+
+        PutObjectPresignRequest presignRequest =
+                PutObjectPresignRequest.builder()
+                        .signatureDuration(Duration.ofMinutes(10))
+                        .putObjectRequest(objectRequest)
+                        .build();
+
+        PresignedPutObjectRequest presignedRequest =
+                awsS3Config.s3Presigner().presignPutObject(presignRequest);
+
+        return presignedRequest.url().toString();
+    }
+}
