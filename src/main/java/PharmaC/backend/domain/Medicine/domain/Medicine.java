c9b2e3af5117471192efccca113f5eede24b1952
chaeshee0908 <chaeshee0908@naver.com>
2023-10-27 16:17:59 +0900

    [chore] 사용하지 않는 import 삭제

--- src/main/java/PharmaC/backend/domain/Medicine/domain/Medicine.java
+++ src/main/java/PharmaC/backend/domain/Medicine/domain/Medicine.java
@@ -1,23 +1,14 @@
 package PharmaC.backend.domain.Medicine.domain;
 
-import PharmaC.backend.domain.History.domain.History;
-import PharmaC.backend.domain.History.vo.HistoryVo;
 import PharmaC.backend.domain.Medicine.dto.MedicineDTO;
-import PharmaC.backend.domain.User.domain.User;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.util.Date;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
