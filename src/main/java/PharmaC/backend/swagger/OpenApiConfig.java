f913d521d18912615cf660885e1d44eb88cae3c4
chaeshee0908 <chaeshee0908@naver.com>
2023-11-15 10:00:44 +0900

    [feat] 스웨거 로컬, 파마씨 서버 요청 경로 설정

--- src/main/java/PharmaC/backend/swagger/OpenApiConfig.java
+++ src/main/java/PharmaC/backend/swagger/OpenApiConfig.java
@@ -3,6 +3,8 @@ package PharmaC.backend.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -13,9 +15,17 @@ public class OpenApiConfig {
     public OpenAPI openAPI() {
         Info info = new Info().title("PHARMA-C API")
                 .description("졸업 프로젝트 파마씨의 API docs 입니다.");
+        Server localServer = new Server();
+        localServer.setDescription("local");
+        localServer.setUrl("http://localhost:8080");
+
+        Server pharmacServer = new Server();
+        pharmacServer.setDescription("test");
+        pharmacServer.setUrl("https://www.hi-pharmac.com");
 
         return new OpenAPI()
                 .components(new Components())
+                .servers(Arrays.asList(localServer, pharmacServer))
                 .info(info);
     }
 }
